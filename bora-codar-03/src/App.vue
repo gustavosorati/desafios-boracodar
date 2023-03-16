8b6f5aff590f6ee9940d0397476a91ccb8f20857
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-03-16 01:04:15 -0300

    desafio-03

---
+++ bora-codar-03/src/App.vue
@@ -0,0 +1,78 @@
+<script setup>
+  import Button from './components/Button.vue'
+</script>
+
+<template>
+  <table id="projeto" cellpadding="0" cellspacing="0">
+    <thead>
+      <tr>
+        <th></th>
+        <th>BOTÃO PRIMÁRIO</th>
+        <th>BOTÃO SECUNDÁRIO</th>
+        <th>BOTÃO TERCIÁRIO</th>
+        <th>CURSOR</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="type">DEFAULT</td>
+        <td><Button text="DEFAULT primary" /></td>
+        <td><Button text="default secondary" variant="secundary" /></td>
+        <td><Button text="DEFAULT tertiary" variant="tertiary" /></td>
+      </tr>
+      <tr>
+        <td class="type">HOVER</td>
+        <td><Button text="HOVER primary" /></td>
+        <td><Button text="HOVER secondary" variant="secundary" /></td>
+        <td><Button text="HOVER tertiary" variant="tertiary" /></td>
+      </tr>
+      <tr>
+        <td class="type">FOCUS</td>
+        <td><Button text="FOCUS primary" isFocused /></td>
+        <td><Button text="FOCUS secondary" variant="secundary" isFocused /></td>
+        <td><Button text="FOCUS tertiary" variant="tertiary" isFocused /></td>
+      </tr>
+      <tr>
+        <td class="type">DISABLED</td>
+        <td><Button text="DISABLED primary" disabled /></td>
+        <td><Button text="DISABLED secondary" variant="secundary" disabled /></td>
+        <td><Button text="DISABLED tertiary" variant="tertiary" disabled /></td>
+      </tr>
+      <tr>
+        <td class="type">LOADING</td>
+        <td><Button text="LOADING primary" isLoading /></td>
+        <td><Button text="LOADING secondary" variant="secundary" isLoading /></td>
+        <td><Button text="LOADING tertiary" variant="tertiary" isLoading /></td>
+      </tr>
+      <tr>
+        <td class="type">MOVABLE</td>
+        <td><Button text="MOVABLE primary" isMovable /></td>
+        <td><Button text="MOVABLE secondary" variant="secundary" isMovable /></td>
+        <td><Button text="MOVABLE tertiary" variant="tertiary" isMovable /></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped>
+  #projeto {
+    border: none;
+  }
+
+  #projeto thead, .type {
+    background-color: #1C162C;
+  }
+
+  #projeto .type {
+    padding: 1rem 2rem;
+  }
+
+  #projeto thead th {
+    padding: 1rem 2rem;
+  }
+
+  #projeto tbody td {
+    text-align: center;
+    padding: 1rem 1rem;
+  } 
+</style>
---
+++ bora-codar-03/src/components/Button.vue
@@ -0,0 +1,98 @@
+<template>
+  <button :class="[
+    variant, 
+    disabled ? 'disabled' : '',
+    isFocused ? 'focus' : '',
+    isLoading ? 'loading' : '',
+    isMovable ? 'movable' : ''
+  ]">
+    <img v-if="isLoading" :class="[isLoading && 'loading']" src="../assets/loading.png" alt="">
+    <img v-if="isMovable" src="../assets/movable.png" alt="">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+  export default {
+    name: "Button",
+    props: {
+      text: String,
+      variant: {
+        type: String,
+        validator: (value) => {
+          return ["primary", "secundary", "tertiary"].includes(value)
+        },
+        default: "primary"
+      },
+      disabled: Boolean,
+      isFocused: Boolean,
+      isLoading: Boolean,
+      isMovable: Boolean
+    },
+  }
+</script>
+
+<style>
+  button {
+    all: unset;
+    padding: 12px 24px;
+    border-radius: 24px;
+    text-transform: uppercase;
+    border: 2px solid transparent;
+  }
+
+  button.disabled {
+    opacity: .6;
+  }
+
+  button.focus {
+    border-color: #d9cdf7;
+  }
+  
+  button.primary {
+    background-color: #8257E5;
+  }
+
+  button.primary:hover {
+    background-color: #9674E5;
+  }
+
+  button.primary:focus {
+    background-color: #9674E5;
+    border-color: #d9cdf7;
+  }
+
+  button.secundary {
+    background-color: #3C3748;
+  }
+
+  button.secundary:hover {
+    background-color: #6A617F;
+  }
+
+  button.tertiary {
+    background-color: transparent;
+  }
+
+  @keyframes rotate {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  button img.loading {
+    animation: rotate 2s ease infinite;
+  }
+
+  button.loading {
+    cursor: wait;
+  }
+
+  button.movable {
+    cursor: grab;
+  }
+</style>
+
